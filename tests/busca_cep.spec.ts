5d03d9a913ed70543c593266d3892db0478accd9
andrade-d <66543988+andrade-d@users.noreply.github.com>
2024-08-20 08:23:32 -0300

    busca_cep
    Implementação do cenário de teste de busca cep.

---
+++ tests/busca_cep.spec.ts
@@ -0,0 +1,12 @@
+import { test, expect } from '@playwright/test';
+
+test('test', async ({ page }) => {
+  await page.goto('https://buscacepinter.correios.com.br/app/endereco/index.php');
+  await page.getByPlaceholder('Não utilize nº de casa/apto/').click();
+  await page.getByPlaceholder('Não utilize nº de casa/apto/').fill('80700000');
+  await page.getByRole('button', { name: 'Buscar' }).click();
+  await page.getByRole('button', { name: 'Buscar' }).click();
+  await page.getByText('Preencha o campo Captcha! (').click();
+  await page.getByRole('button', { name: 'Buscar' }).click();
+  await expect(page.getByText('Preencha o campo Captcha! (')).toBeVisible();
+});
