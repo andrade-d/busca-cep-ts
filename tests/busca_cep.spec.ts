e66bf52fbee664a727a8af762d5c6f9cdc1c5640
andrade-d <66543988+andrade-d@users.noreply.github.com>
2024-08-20 08:41:07 -0300

    headless

--- tests/busca_cep.spec.ts
+++ tests/busca_cep.spec.ts
@@ -1,6 +1,6 @@
 import { test, expect } from '@playwright/test';
 
-test('test', async ({ page }) => {
+test('busca_cep', async ({ page }) => {
   await page.goto('https://buscacepinter.correios.com.br/app/endereco/index.php');
   await page.getByPlaceholder('Não utilize nº de casa/apto/').click();
   await page.getByPlaceholder('Não utilize nº de casa/apto/').fill('80700000');
@@ -9,4 +9,6 @@ test('test', async ({ page }) => {
   await page.getByText('Preencha o campo Captcha! (').click();
   await page.getByRole('button', { name: 'Buscar' }).click();
   await expect(page.getByText('Preencha o campo Captcha! (')).toBeVisible();
+  await page.close(); 
 });
+
