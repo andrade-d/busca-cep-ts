eb059c4a5a6d3447ffb532a1609ecce6a89c0e5a
andrade-d <66543988+andrade-d@users.noreply.github.com>
2024-08-20 12:44:56 -0300

    update

--- tests/busca_cep.spec.ts
+++ tests/busca_cep.spec.ts
@@ -1,14 +1,16 @@
 import { test, expect } from '@playwright/test';
 
-test('busca_cep', async ({ page }) => {
-  await page.goto('https://buscacepinter.correios.com.br/app/endereco/index.php');
-  await page.getByPlaceholder('Não utilize nº de casa/apto/').click();
-  await page.getByPlaceholder('Não utilize nº de casa/apto/').fill('80700000');
-  await page.getByRole('button', { name: 'Buscar' }).click();
-  await page.getByRole('button', { name: 'Buscar' }).click();
-  await page.getByText('Preencha o campo Captcha! (').click();
-  await page.getByRole('button', { name: 'Buscar' }).click();
-  await expect(page.getByText('Preencha o campo Captcha! (')).toBeVisible();
-  await page.close(); 
+test('test', async ({ page }) => {
+  try {
+    await page.goto('https://buscacepinter.correios.com.br/app/endereco/index.php');
+    await page.getByRole('heading', { name: 'Busca CEP' }).click();
+    await page.getByPlaceholder('Não utilize nº de casa/apto/').click();
+    await page.getByPlaceholder('Não utilize nº de casa/apto/').fill('80700000');
+    await page.getByRole('button', { name: 'Buscar' }).click();
+    await page.getByText('Preencha o campo Captcha! (').click();
+  } catch (error) {
+    console.error('An error occurred:', error);
+  } finally {
+    await page.close();
+  }
 });
-
